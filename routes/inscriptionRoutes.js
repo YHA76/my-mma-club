16081056a436a3fd15a3d29b69c956120f6494fe
YHA76 <yahyabnlpro@gmail.com>
2025-03-11 12:46:47 +0100

    add anti spam

--- routes/inscriptionRoutes.js
+++ routes/inscriptionRoutes.js
@@ -2,14 +2,43 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 
+const recentSubmissions = new Map(); // Stocke les inscriptions récentes (temporaire en RAM)
+
 router.post("/", async (req, res) => {
   const { nom, prenom, photo, age, moyenPaiement, email } = req.body;
+  const moyensPaiementValides = ["Carte bancaire", "Espèce"];
+
+  // Vérifier si l'utilisateur a déjà soumis récemment
+  const emailKey = email.toLowerCase();
+  if (recentSubmissions.has(emailKey)) {
+    return res.status(429).json({
+      error:
+        "Vous avez déjà soumis une inscription récemment. Veuillez patienter.",
+    });
+  }
+
+  // Ajouter l'email à la liste des soumissions récentes (Expire après 10 minutes)
+  recentSubmissions.set(emailKey, Date.now());
+  setTimeout(() => recentSubmissions.delete(emailKey), 10 * 60 * 1000);
 
   // Vérification des champs obligatoires
   if (!nom || !prenom || !photo || !age || !moyenPaiement || !email) {
     return res
       .status(400)
       .json({ error: "Tous les champs doivent être remplis." });
+  }
+
+  // Vérification des champs obligatoires avec les limites de caractères
+  if (!nom || nom.length < 3 || nom.length > 50) {
+    return res
+      .status(400)
+      .json({ error: "Le nom doit contenir entre 3 et 50 caractères." });
+  }
+
+  if (!prenom || prenom.length < 3 || prenom.length > 50) {
+    return res
+      .status(400)
+      .json({ error: "Le prénom doit contenir entre 3 et 50 caractères." });
   }
 
   // Vérifier que l'email est valide
@@ -20,11 +49,18 @@ router.post("/", async (req, res) => {
       .json({ error: "Veuillez entrez une adresse mail valide." });
   }
 
-  // Vérifier que l'âge est un nombre positif
-  if (isNaN(age) || age <= 0) {
+  // Vérifier que l'âge est un âge valide
+  if (!age || isNaN(age) || age < 6 || age > 80) {
     return res
       .status(400)
-      .json({ error: "L'âge doit être un nombre positif." });
+      .json({ error: "L'âge doit être un nombre entre 6 et 80 ans." });
+  }
+
+  // Vérifier que le moyen de paiement est valide
+  if (!moyenPaiement || !moyensPaiementValides.includes(moyenPaiement)) {
+    return res.status(400).json({
+      error: "Le moyen de paiement doit être 'Carte bancaire' ou 'Espèce'.",
+    });
   }
 
   // Configurer le transporteur d'e-mail
